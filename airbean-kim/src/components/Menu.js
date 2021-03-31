61e314562a0ffe3764f6f5359cbb1013d468f319
sayttplx <sayttplx@gmail.com>
2021-03-31 18:04:59 +0200

    static profile

--- airbean-kim/src/components/Menu.js
+++ airbean-kim/src/components/Menu.js
@@ -3,7 +3,7 @@ const Menu = () => {
         <div>
             <h1>Meny</h1>
   <div>
-      <img width="30px" height="30px" src="../svg/add.svg" alt />
+      <img width="30px" height="30px" src="../svg/add.svg" alt="add" />
     
     <h3>Bryggkaffe</h3>
     <hr/>
@@ -11,7 +11,7 @@ const Menu = () => {
     <h3>49kr</h3>
   </div>
   <div>
-      <img width="30px" height="30px" src="../svg/add.svg" alt />
+      <img width="30px" height="30px" src="../svg/add.svg" alt="add" />
     
     <h3>Caffè Doppio</h3>
     <hr/>
@@ -19,7 +19,7 @@ const Menu = () => {
     <h3>49kr</h3>
   </div>
   <div>
-      <img width="30px" height="30px" src="../svg/add.svg" alt />
+      <img width="30px" height="30px" src="../svg/add.svg" alt="add" />
     
     <h3>Cappuccino</h3>
     <hr/>
@@ -27,7 +27,7 @@ const Menu = () => {
     <h3>49kr</h3>
   </div>
   <div>
-      <img width="30px" height="30px" src="../svg/add.svg" alt />
+      <img width="30px" height="30px" src="../svg/add.svg" alt="add" />
     
     <h3>Latte Macchiato</h3>
     <hr/>
@@ -35,7 +35,7 @@ const Menu = () => {
     <h3>49kr</h3>
   </div>
   <div>
-      <img width="30px" height="30px" src="../svg/add.svg" alt />
+      <img width="30px" height="30px" src="../svg/add.svg" alt="add" />
     
     <h3>Kaffe Latte</h3>
     <hr/>
@@ -43,7 +43,7 @@ const Menu = () => {
     <h3>49kr</h3>
   </div>
   <div>
-      <img width="30px" height="30px" src="../svg/add.svg" alt />
+      <img width="30px" height="30px" src="../svg/add.svg" alt="add" />
     
     <h3>Cortado</h3>
     <hr/>
--- airbean-kim/src/components/Profile.js
+++ airbean-kim/src/components/Profile.js
@@ -1,10 +1,31 @@
 const Profile = () => {
     return ( 
         <div>
+            
             <h1>PROFILE</h1>
             <h2>Sixten Kaffelövér</h2>
             <p>sixten.kaffelover@zocom.se</p>
             <h2>Orderhistorik</h2>
+            <br/> <br/>
+            <div>
+            <p>#AB1123282323Z 20/03/06  </p>
+            <p>total ordersumma 443 kr</p>
+            </div>
+            <hr/>
+            <div>
+            <p>##AB1128382323X 20/03/06</p>
+            <p> total ordersumma 333 kr kr</p>
+            </div>
+            <hr/>
+            <div>
+            <p>#AB1444482323X 20/03/06 </p>
+            <p> Total ordersumma 893 kr</p>
+            </div>
+            <hr/>
+            <div>
+            <p>Totalt spenderat 1669 kr</p>
+            
+            </div>
         </div>
      );
 }
